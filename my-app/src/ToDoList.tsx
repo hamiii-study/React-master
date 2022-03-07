4574d4f6f1424cfdad0bb8b64404cfab66ef9008
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-07 13:58:01 +0900

    Form 2

--- my-app/src/ToDoList.tsx
+++ my-app/src/ToDoList.tsx
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
     setToDo(value);
+    setToDoError("");
   };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
         <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
       </form>
     </div>
   );
