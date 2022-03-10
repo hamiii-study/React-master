a0e2b22f04d28d51c9ba598b1358b16952fead01
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-10 20:02:32 +0900

    MotionValues 3

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,20 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import { useRef } from "react";
 import React from "react";
 import { useEffect } from "react";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: purple;
 `;
 const Box = styled(motion.div)`
   width: 200px;
@@ -60,15 +64,28 @@ const BiggerBox = styled.div`
 
 function App() {
   const x = useMotionValue(0); //tract value
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useEffect(() => {
-    // x.onChange(() => console.log(x.get()));
-    scale.onChange(() => console.log(scale.get()));
-  }, [x]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 238))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  // useEffect(() => {
+  //   // x.onChange(() => console.log(x.get()));
+  //   // rotateZ.onChange(() => console.log(rotateZ.get()));
+  //   scrollY.onChange(() => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollYProgress, scrollY]);
   return (
-    <Wrapper>
+    <Wrapper style={{ background: gradient }}>
       <button onClick={() => x.set(200)}>Click me</button>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
