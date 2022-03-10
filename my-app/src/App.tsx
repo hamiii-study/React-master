cbc21d7066835f5ebad7ed872240595f362422d1
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-10 11:19:50 +0900

    Variants 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,25 +7,69 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: aqua;
+  background-color: purple;
 `;
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
+
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+
+  /* display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center; */
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 `;
 
-//Variants => stage
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const Circle = styled(motion.div)`
+  width: 70%;
+  height: 70%;
+  background-color: white;
+  border-radius: 50%;
+  place-self: center;
+`;
+
+const boxVariants = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.15,
+    },
+  },
 };
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* box의 initial, animate는 circle에 상속됨 */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
