e719ee6f6d3f332019fc42bba789b4573bf64cee
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-10 20:43:12 +0900

    SVG animation

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,56 +2,11 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-
-  /* display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: center; */
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-`;
-
-const Circle = styled(motion.div)`
-  width: 70%;
-  height: 70%;
-  background-color: white;
-  border-radius: 50%;
-  place-self: center;
-`;
-
-const boxVariants = {
-  hover: {
-    scale: 1.2,
-    rotateZ: 90,
-  },
-  tap: {
-    borderRadius: " 100px",
-    scale: 1,
-  },
-};
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
 `;
 
 const Svg = styled.svg`
