f130e6fd973196c70a7a4e9e46d0338a386c63b6
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-10 11:25:18 +0900

    gestures-hover&tap

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
 
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -34,42 +34,20 @@ const Circle = styled(motion.div)`
 `;
 
 const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
+  hover: {
+    scale: 1.5,
+    rotateZ: 90,
+  },
+  tap: {
+    borderRadius: " 100px",
     scale: 1,
-    transition: {
-      type: "spring",
-      delay: 0.5,
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.15,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        {/* box의 initial, animate는 circle에 상속됨 */}
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box variants={boxVariants} whileHover="hover" whileTap="tap" />
     </Wrapper>
   );
 }
