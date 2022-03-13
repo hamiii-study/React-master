bf7154dab2d19e663cf2a9e9ae2d8372dd662656
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-13 15:47:04 +0900

    challenge-solution

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,130 +1,164 @@
-import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import styled from "styled-components";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   height: 100vh;
-  width: 100%;
+  width: 100vw;
+  box-sizing: border-box;
+  background: linear-gradient(135deg, #e09, #d0e);
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  position: relative;
+  padding: 0 50px;
 `;
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  width: 40vw;
-  gap: 20px;
-
-  margin-bottom: 50px;
+  grid-template-rows: 300px 300px;
+  gap: 15px;
+  max-width: 890px;
+  width: 100%;
 `;
 
 const Box = styled(motion.div)`
-  height: 200px;
-
-  background-color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(255, 255, 255, 0.6);
   border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.4), 0 0px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Button = styled(motion.span)`
+  cursor: pointer;
+  padding: 10px;
+  margin-top: 50px;
+  border-radius: 5px;
+  color: white;
+  background-color: white;
+  font-weight: 600;
+  font-size: 18px;
+  position: absolute;
+  bottom: 75px;
 `;
 
 const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
-  position: absolute;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-const Circle = styled(motion.div)`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
-  background-color: white;
+  position: absolute;
+  top: 0;
 `;
 
-const Button = styled(motion.button)``;
+const BigBox = styled(motion.div)`
+  background-color: white;
+  width: 400px;
+  height: 300px;
+  border-radius: 10px;
+`;
+
 const buttonVariants = {
-  hover: {
-    color: "rgb(255,150,100)",
+  zero: {
+    color: "#0052D4",
+    transform: "scale(1)",
+  },
+  one: {
+    color: "#ff6a00",
+    transform: "scale(1.2)",
   },
 };
 
 const boxVariants = {
-  hover: { scaleY: 1.1, backgroundColor: "rgb(255, 204, 204)", opacity: 0.8 },
+  initial: (i) => ({
+    scale: 1,
+    x: 0,
+    y: 0,
+  }),
+  hover: (i) => ({
+    scale: 1.1,
+    y: i ? -15 : 15,
+    x: i ? -20 : 20,
+  }),
 };
 
-function App() {
-  const [id, setId] = useState<null | string>(null);
-  const [clicked, setClicked] = useState(false);
+const overlayVariants = {
+  hidden: {
+    backgroundColor: "rgba(0, 0, 0, 0.0)",
+  },
+  visible: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+};
+
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [selectedBox, setSelectedBox] = useState(null);
+  const onToggleClick = () => {
+    setIndex((prev) => (prev === 1 ? 0 : 1));
+  };
+  const onClickBox = (i) => {
+    setSelectedBox(i);
+  };
   return (
     <Wrapper>
       <Grid>
         <Box
-          variants={boxVariants}
-          whileHover="hover"
-          onClick={() => setId("1")}
+          onClick={() => onClickBox("1")}
           layoutId="1"
-        />
-        <Box>
-          {!clicked ? (
-            <Circle
-              layoutId="circle"
-              style={{
-                borderRadius: 0,
-                backgroundColor: "rgb(126, 255, 245)",
-                scale: 1,
-              }}
-            />
-          ) : null}
-        </Box>
-        <Box>
-          {clicked ? (
-            <Circle
-              layoutId="circle"
-              style={{
-                borderRadius: 0,
-                backgroundColor: "rgb(24, 220, 255)",
-                scale: 1.3,
-              }}
-            />
-          ) : null}
-        </Box>
-        <Box
           variants={boxVariants}
+          custom={1}
+          initial="initial"
           whileHover="hover"
-          onClick={() => setId("2")}
-          layoutId="2"
+        />
+        <Box>{index === 0 ? <Circle layoutId="circle" /> : null}</Box>
+        <Box>{index === 1 ? <Circle layoutId="circle" /> : null}</Box>
+        <Box
+          onClick={() => onClickBox("0")}
+          variants={boxVariants}
+          custom={0}
+          layoutId="0"
+          initial="initial"
+          whileHover="hover"
         />
       </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
-          >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
       <Button
-        onClick={() => setClicked((prev) => !prev)}
+        animate={index === 1 ? "one" : "zero"}
         variants={buttonVariants}
-        whileHover="hover"
+        initial="zero"
+        onClick={onToggleClick}
       >
         Switch
       </Button>
+      <AnimatePresence>
+        {selectedBox ? (
+          <Overlay
+            variants={overlayVariants}
+            animate="visible"
+            exit="hidden"
+            onClick={() => onClickBox(null)}
+          >
+            <BigBox layoutId={selectedBox}></BigBox>
+          </Overlay>
+        ) : (
+          ""
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
-export default App;
-
-// drag="x" -> x축으로만 움직일 수 있음!
-//  overflow: hidden; ->밖으로 나가면 안보임
