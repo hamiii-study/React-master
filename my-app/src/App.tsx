cf5d654a5db9f372dda9f5566fc42387e4405707
haemiii <73984822+haemiii@users.noreply.github.com>
2022-03-13 15:41:19 +0900

    challenge

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,26 +6,30 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100%;
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
-  gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
+  grid-template-columns: repeat(2, 1fr);
+  width: 40vw;
+  gap: 20px;
+
+  margin-bottom: 50px;
 `;
 
 const Box = styled(motion.div)`
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Overlay = styled(motion.div)`
@@ -37,15 +41,66 @@ const Overlay = styled(motion.div)`
   justify-content: center;
   align-items: center;
 `;
+const Circle = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: white;
+`;
+
+const Button = styled(motion.button)``;
+const buttonVariants = {
+  hover: {
+    color: "rgb(255,150,100)",
+  },
+};
+
+const boxVariants = {
+  hover: { scaleY: 1.1, backgroundColor: "rgb(255, 204, 204)", opacity: 0.8 },
+};
 
 function App() {
   const [id, setId] = useState<null | string>(null);
+  const [clicked, setClicked] = useState(false);
   return (
     <Wrapper>
       <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
+        <Box
+          variants={boxVariants}
+          whileHover="hover"
+          onClick={() => setId("1")}
+          layoutId="1"
+        />
+        <Box>
+          {!clicked ? (
+            <Circle
+              layoutId="circle"
+              style={{
+                borderRadius: 0,
+                backgroundColor: "rgb(126, 255, 245)",
+                scale: 1,
+              }}
+            />
+          ) : null}
+        </Box>
+        <Box>
+          {clicked ? (
+            <Circle
+              layoutId="circle"
+              style={{
+                borderRadius: 0,
+                backgroundColor: "rgb(24, 220, 255)",
+                scale: 1.3,
+              }}
+            />
+          ) : null}
+        </Box>
+        <Box
+          variants={boxVariants}
+          whileHover="hover"
+          onClick={() => setId("2")}
+          layoutId="2"
+        />
       </Grid>
       <AnimatePresence>
         {id ? (
@@ -59,6 +114,13 @@ function App() {
           </Overlay>
         ) : null}
       </AnimatePresence>
+      <Button
+        onClick={() => setClicked((prev) => !prev)}
+        variants={buttonVariants}
+        whileHover="hover"
+      >
+        Switch
+      </Button>
     </Wrapper>
   );
 }
